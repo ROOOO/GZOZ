8120bf422172aa4b20226f6a0b8c069d777aef7a
ROOOO <liam.oh.work@gmail.com>
2024-10-18 20:05:22 +0800

    [HW2] tone mapping colour from linear to sRGB

--- homework2/src/shaders/prtShader/prtFragment.glsl
+++ homework2/src/shaders/prtShader/prtFragment.glsl
@@ -4,6 +4,22 @@ precision mediump float;
 
 varying highp vec3 vColor;
 
+vec3 toSRGB(vec3 color) {
+  vec3 result;
+
+  for (int i = 0; i < 3; ++i) {
+    float value = color[i];
+
+    if (value <= 0.0031308) {
+      result[i] = 12.92 * value;
+    } else {
+      result[i] = (1.0 + 0.055) * pow(value, 1.0 / 2.4) - 0.055;
+    }
+  }
+
+  return result;
+}
+
 void main(void) {
-  gl_FragColor = vec4(vColor, 1.0);
+  gl_FragColor = vec4(toSRGB(vColor), 1.0);
 }
